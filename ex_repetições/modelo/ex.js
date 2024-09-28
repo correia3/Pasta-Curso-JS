3e2b9daacacac141133f3c5b6ec9541e77941096
Arthur Correia <arthurc.estudos3@gmail.com>
2024-09-28 14:59:10 -0300

    atividade 14 - 01 (pronto)!

--- ex_repetições/modelo/ex.js
+++ ex_repetições/modelo/ex.js
@@ -1,13 +1,39 @@
 
 function contar(){
-    var inicio = document.getElementById('Inicio').value;
-    var fim = document.getElementById('Fim').value;
-    var passo = document.getElementById('Passo').value;
-    
+    let inicio = document.getElementById('Inicio');
+    let fim = document.getElementById('Fim');
+    let passo = document.getElementById('Passo');
     let clicarBotao = document.getElementById('botao');
-    let paragrafo = document.getElementById('paragrafo');
+    let pre = document.getElementById('pre');
 
-    for (inicio; inicio <= fim ; passo){
-        paragrafo.innerHTML = `<p id="paragrafo"> ${inicio}</p>`;
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        pre.innerHTML = 'Impossível contar!';
+
+    } else {
+        pre.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        
+        if(p <= 0) {
+            alert('Passo Inválido! Considerando passo 1.')
+            p = 1
+        }
+
+        if (i < f ){
+            // contagem progressiva
+            for (let c = i; c <= f ; c += p){
+                pre.innerHTML += ` ${c}`;
+            }
+    
+        } else {
+            // contagem regressiva
+            for (let c = i ; c >= f ; c -= p){
+                pre.innerHTML += ` ${c} \u{1F449}` ;
+            }
+            
+        }
+        pre.innerHTML += `\u{1F3C1}`;
     }
+
 }
